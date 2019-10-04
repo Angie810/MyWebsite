d466e046aac5907d1f6cc0c8fc8b58f239ea374b
Angie <you@example.com>
2019-10-04 14:46:16 +0200

    changes

---
+++ myweb/src/components/NavbarMain.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link } from 'react-router-dom'
+import icon from '../icon home.png'
+import styled from 'styled-components'
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
+
+function NavbarMain(){
+    return(
+      
+        <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/"><img src={icon} alt="home" width={35} height={30}/></Navbar.Brand>
+        <Nav className="mr-auto">
+               <Nav.Link href="/CV">CV</Nav.Link>
+               <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+               <Nav.Link href="/About">About</Nav.Link>
+               <Nav.Link href="/Contact">Contact</Nav.Link>
+         </Nav> 
+           
+        </Navbar>
+      
+    );
+}
+export default NavbarMain;
+
+const ButtonContainer = styled.button`
+text-transformation:capitalized
+`
