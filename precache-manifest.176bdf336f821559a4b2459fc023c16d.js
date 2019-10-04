d7764690d3504ffaa6fdc578491684d119f6b716
Angie <you@example.com>
2019-10-04 16:33:18 +0200

    Updates

---
+++ precache-manifest.176bdf336f821559a4b2459fc023c16d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a498da0122ea91de3fdb537a98ca5f",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "e368b74e4fc414a3cecc",
+    "url": "/MyWebsite/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "36562c7ab673686106ca",
+    "url": "/MyWebsite/static/css/main.b883fa5c.chunk.css"
+  },
+  {
+    "revision": "e368b74e4fc414a3cecc",
+    "url": "/MyWebsite/static/js/2.c5d29ef9.chunk.js"
+  },
+  {
+    "revision": "36562c7ab673686106ca",
+    "url": "/MyWebsite/static/js/main.69cf9776.chunk.js"
+  },
+  {
+    "revision": "9b4775d183b1679427d1",
+    "url": "/MyWebsite/static/js/runtime-main.ccc0b7df.js"
+  },
+  {
+    "revision": "d97b0c340f3a26da09ca9aa860ac53a5",
+    "url": "/MyWebsite/static/media/angiecv.d97b0c34.pdf"
+  },
+  {
+    "revision": "e95329dabc8f38078f880946e1e26fe7",
+    "url": "/MyWebsite/static/media/coding1.e95329da.png"
+  },
+  {
+    "revision": "d7771524124dea2614a7618a96e31fb2",
+    "url": "/MyWebsite/static/media/computerImage.d7771524.jpeg"
+  },
+  {
+    "revision": "5beaa476afb2cd82c48e44d2b34757bd",
+    "url": "/MyWebsite/static/media/design1.5beaa476.jpg"
+  },
+  {
+    "revision": "121c8138a484bf9f9663173a7c0163a8",
+    "url": "/MyWebsite/static/media/india.121c8138.jpg"
+  },
+  {
+    "revision": "a551cf76c35f83887cc0a817e300cafa",
+    "url": "/MyWebsite/static/media/insta.a551cf76.jpg"
+  },
+  {
+    "revision": "3c28d920d78e4939f3b2ecbc4f6dd973",
+    "url": "/MyWebsite/static/media/pic1.3c28d920.jpg"
+  }
+]);
